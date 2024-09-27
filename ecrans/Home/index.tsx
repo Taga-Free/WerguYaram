4926c62bf637bb393be58a0ac0333dc991a8198f
Taga-Free <tagatheking@gmail.com>
2024-09-27 15:43:11 +0000

    Fin de l'ecran dashboard

--- ecrans/Home/index.tsx
+++ ecrans/Home/index.tsx
@@ -1,95 +1,79 @@
-import { View, Text ,ScrollView , Image , FlatList, TouchableOpacity} from 'react-native'
-import React from 'react'
+import { View, Text, ScrollView, Image, FlatList, TouchableOpacity } from 'react-native';
+import React, { useState } from 'react';
 import dashboardStyles from './style';
 import { fakeActivity } from '../../fakeData/fakeActivity';
-import { Item } from 'react-native-paper/lib/typescript/components/Drawer/Drawer';
 import ActivityItems from '../../composantes/ActivityItems';
 import { fakeSynthomes } from '../../fakeData/fakeSynthome';
 import Synthomes from '../../composantes/SynthomeItems';
 import { fakeDoctor } from '../../fakeData/fakeDoctor';
 
 const Home = () => {
+  // State pour gérer l'affichage de toute la liste ou non
+  const [showAllDoctors, setShowAllDoctors] = useState(false);
+
+  // Fonction pour basculer entre l'affichage partiel et complet
+  const handleShowAllDoctors = () => {
+    setShowAllDoctors(!showAllDoctors); // Alterne entre afficher tout et partiel
+  };
+
+  // Gère combien de docteurs afficher
+  const displayedDoctors = showAllDoctors ? fakeDoctor : fakeDoctor.slice(0, 3);
+
   return (
     <ScrollView>
       <View style={dashboardStyles.header}>
-          {/*Debut du header*/}
-          <Text style={dashboardStyles.userName}>Doctor M.Gueye</Text>
+        {/*Début du header*/}
+        <Text style={dashboardStyles.userName}>Doctor M.Gueye</Text>
         <View style={dashboardStyles.imageContainer}>
-          <Image source={require('./../../assets/doctor.jpeg')} style={dashboardStyles.userImg}/>
+          <Image source={require('./../../assets/doctor.jpeg')} style={dashboardStyles.userImg} />
         </View>
       </View>
-      
-      {/*Fin du header*/}
-      
-      {/*Liste des Activités*/}
 
-        <FlatList 
+      {/* Liste des Activités */}
+      <FlatList
         data={fakeActivity}
-        keyExtractor={item => item.id}
+        keyExtractor={(item) => item.id.toString()}
         horizontal={true}
-        showsHorizontalScrollIndicator= {false}
+        showsHorizontalScrollIndicator={false}
         style={dashboardStyles.scrollableList}
-        renderItem={({item}) => {
-          return(
-            <ActivityItems item={item}/>
-          );
-        }}
-        />
-      {/*Fin de Liste des Activités*/}
+        renderItem={({ item }) => <ActivityItems item={item} />}
+      />
 
-      
-      {/*Liste des Synthomes*/}
+      {/* Liste des Synthomes */}
       <View style={dashboardStyles.title}>
-        <Text style={dashboardStyles.titleBold} >Quel symthöme avez-vous?</Text>
+        <Text style={dashboardStyles.titleBold}>Quel symthöme avez-vous?</Text>
       </View>
-      <FlatList 
+      <FlatList
         data={fakeSynthomes}
-        keyExtractor={item => item.id}
+        keyExtractor={(item) => item.id.toString()}
         horizontal={true}
-        showsHorizontalScrollIndicator= {false}
+        showsHorizontalScrollIndicator={false}
         style={dashboardStyles.scrollableList}
-        renderItem={({item}) => {
-          return(
-            <Synthomes item={item}/>
-          );
-        }}
-        />
+        renderItem={({ item }) => <Synthomes item={item} />}
+      />
 
-        
-      {/*Fin de Liste des Synthomes*/}
-
-      
-      {/*Docteur List*/}
-      
-        <View style={dashboardStyles.title_space_between}>
-          <Text style={dashboardStyles.titleBold} >Nos Docteurs</Text>
-          <TouchableOpacity>
-            <Text style={dashboardStyles.link}>Afficher tout...</Text>
-        
-      </TouchableOpacity>
-        </View>
-        <View style={dashboardStyles.DoctorContainer}>
-        {
-          fakeDoctor.splice(0, 5).map((doctor, index) =>{
-            return(
-              <TouchableOpacity 
-              key={doctor.id} 
-              style={dashboardStyles.DoctorCard}>
-                <Image 
-                source={{uri: `${doctor.img}`}}
-                style={dashboardStyles.DoctorImg}/>
-                <View style={dashboardStyles.DoctorInfos}>
-                <Text style={dashboardStyles.DoctorFullName}>{doctor.fullName}</Text>
-                <Text style={dashboardStyles.DoctorSpeciality}>{doctor.speciality}</Text>
-                </View>
-              </TouchableOpacity>
-            )
-          })
-        }
-
-        </View>
+      {/* Docteur List */}
+      <View style={dashboardStyles.title_space_between}>
+        <Text style={dashboardStyles.titleBold}>Nos Docteurs</Text>
+        <TouchableOpacity onPress={handleShowAllDoctors}>
+          <Text style={dashboardStyles.link}>
+            {showAllDoctors ? 'Afficher moins...' : 'Afficher plus...'}
+          </Text>
+        </TouchableOpacity>
+      </View>
+      <View style={dashboardStyles.DoctorContainer}>
+        {displayedDoctors.map((doctor) => (
+          <TouchableOpacity key={doctor.id} style={dashboardStyles.DoctorCard}>
+            <Image source={{ uri: `${doctor.img}` }} style={dashboardStyles.DoctorImg} />
+            <View style={dashboardStyles.DoctorInfos}>
+              <Text style={dashboardStyles.DoctorFullName}>{doctor.fullName}</Text>
+              <Text style={dashboardStyles.DoctorSpeciality}>{doctor.speciality}</Text>
+            </View>
+          </TouchableOpacity>
+        ))}
+      </View>
     </ScrollView>
-  )
-}
+  );
+};
 
 export default Home;
